2717e2a7b4b37e369028da781ad9b5e12d6efe93
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-01-31 15:49:17 +0900

    springboot파일 생성

---
+++ dailymoon/src/test/java/com/example/dailymoon/DailymoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailymoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailymoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
