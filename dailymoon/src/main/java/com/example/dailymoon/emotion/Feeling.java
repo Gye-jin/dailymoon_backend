b7deaa4759ba9237deea5b354c8c19a79f11c8c0
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-02-10 15:43:16 +0900

    메인 당겨옴

--- dailymoon/src/main/java/com/example/dailymoon/emotion/Feeling.java
+++
@@ -1,9 +0,0 @@
-package com.example.dailymoon.emotion;
-
-public enum Feeling {
-	Great,
-	Good,
-	Fine,
-	Bad,
-	Worst
-}
---
+++ dailymoon/src/main/java/com/example/dailymoon/service/JWTServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.dailymoon.service;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.dailymoon.config.JwtProperties;
+import com.example.dailymoon.entity.Member;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class JWTServiceImpl implements JWTService {
+
+	@Override
+	public String createToken(Member member) {
+		System.out.println("######## createToken User : " + member);
+
+		String jwtToken = JWT.create().withSubject(member.getNickname())
+				.withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
+				.withClaim("id", member.getUserId()).withClaim("password", member.getPassword())
+				.sign(Algorithm.HMAC512(JwtProperties.SECRET));
+
+		return jwtToken;
+	}
+
+}
